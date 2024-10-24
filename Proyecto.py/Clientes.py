13ec101bcfc3b1c1d9f27c5634dc37e5c7435d49
Juan Moreira <j.moreira@correo.unimet.edu.ve>
2024-10-24 09:04:38 -0400

    Update Clientes.py

--- Proyecto.py/Clientes.py
+++ Proyecto.py/Clientes.py
@@ -2,22 +2,21 @@
 
 class Cliente:
         #Constructor de la clase Cliente
-    def __init__(self, nombre, apellido, cedula, rif, correo_electronico, telefono):
+    def __init__(self, nombre, apellido, cedula, correo_electronico, telefono):
         self.nombre = nombre
         self.apellido = apellido
         self.cedula = cedula
-        self.rif = rif
         self.correo = correo_electronico
         self.telefono = telefono
         
     def show_attr(self):
         print(f"""
               Nombre = {self.nombre + self.apellido}
-              Cedula = {self.cedula or self.rif}
+              Cedula = {self.cedula}
               Correo Electronico = {self.correo}
               Telefono = {self.telefono}
               """)
         
 class Juridico(Cliente):
-    def __init__(self, nombre, apellido, cedula, rif, correo_electronico, telefono):
-        super().__init__(nombre, apellido, cedula, rif, correo_electronico, telefono)
+    def __init__(self, nombre, apellido, cedula, correo_electronico, telefono):
+        super().__init__(nombre, apellido, cedula, correo_electronico, telefono)
