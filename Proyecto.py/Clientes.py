ef6a65d6c0f1e79a048d1e0936cb860ce3816f71
Juan Moreira <j.moreira@correo.unimet.edu.ve>
2024-11-07 22:09:21 -0400

    Update Clientes.py

--- Proyecto.py/Clientes.py
+++ Proyecto.py/Clientes.py
@@ -1,7 +1,14 @@
 #Constructor, metodos y atributo de la clase Clientes
-
+from dataclasses import dataclass
+@dataclass
 class Cliente:
         #Constructor de la clase Cliente
+    nombre : str 
+    apellido: str 
+    cedula: int
+    correo: str 
+    telefono : str 
+       
     def __init__(self, nombre, apellido, cedula, correo_electronico, telefono):
         self.nombre = nombre
         self.apellido = apellido
@@ -11,14 +18,20 @@ class Cliente:
         
     def show_attr(self):
         print(f"""
-              Nombre = {self.nombre + self.apellido}
+              Nombre = {self.nombre}
+              Apellido = {self.apellido}
               Cedula = {self.cedula}
               Correo Electronico = {self.correo}
               Telefono = {self.telefono}
               """)
         
+@dataclass
 class Juridico(Cliente):
-    def __init__(self, nombre_juridico,rif, correo_electronico, telefono):
+    
+    nombre_juridico: str
+    rif: int
+    
+    def __init__(self, nombre_juridico, rif, correo_electronico, telefono):
         super().__init__("", "", "", correo_electronico, telefono)
         self.nombre_juridico = nombre_juridico
         self.rif = rif
