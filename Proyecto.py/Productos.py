73813be05b054c6b7ada3c20e186842bf097775f
Juan Moreira <j.moreira@correo.unimet.edu.ve>
2024-11-07 22:11:08 -0400

    Update Productos.py

--- Proyecto.py/Productos.py
+++ Proyecto.py/Productos.py
@@ -1,24 +1,33 @@
 #Constructor, metodos y atributo de la clase Productos
 
+from dataclasses import dataclass
+@dataclass
 class Producto:
     #Constructor de la clase Producto
-    def __init__(self, nombre, descripción, precio, categoria, inventario):
+    
+    nombre: str
+    descripción: str
+    precio: float
+    categoria: str
+    inventario: str
+    modelo_vehiculo: list
+    
+    def __init__(self, nombre, descripción, precio, categoria, inventario, modelo_vehiculo):
         self.nombre = nombre
         self.descripción = descripción
         self.precio = precio
         self.categoria = categoria
         self.inventario = inventario
+        self.modelo_vehiculo = modelo_vehiculo
     
     def show_attr(self):
+        #Muestra en la consola los datos del cliente
         print(f"""
               Nombre del Producto = {self.nombre}
               Descripción = {self.descripción}
               Precio = {self.precio}
               Categoria = {self.categoria}
               Inventario = {self.inventario}
+              Modelo de Vehiculo = {self.modelo_vehiculo}
               """)
-        
-class Buscar(Producto):
-    def __init__(self, nombre, descripción, precio, categoria, inventario, disponibilidad):
-        super().__init__(nombre, descripción, precio, categoria, inventario)
-        self.disponibilidad = disponibilidad
+    
