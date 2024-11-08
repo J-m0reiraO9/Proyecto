a2eb5984f9447ab641a8050cdc5b60a82c458d3e
Juan Moreira <j.moreira@correo.unimet.edu.ve>
2024-11-07 22:09:55 -0400

    Update Estadistica.py

--- Proyecto.py/Estadistica.py
+++ Proyecto.py/Estadistica.py
@@ -1,3 +1,21 @@
-class Estadistica:
-    def __init__(self) -> None:
-        pass
+#Constructor, metodos y atributos de la clase Indicadores de gestion
+from dataclasses import dataclass
+@dataclass
+class Indicadores_de_gestion:
+    #Constructor de la clase Indicadores de Gestion
+    
+    informes_ventas = str
+    informes_pago = str
+    informes_envio = str
+
+    def __init__(self, informes_ventas, informes_pago, informe_envio):
+        self.informes_ventas = informes_ventas
+        self.informes_pago = informes_pago
+        self.informes_envio = informe_envio
+        
+    def show_attr(self):
+        print(f"""
+              Informes de ventas = {self.informes_ventas}
+              Informes de pago = {self.informes_pago}
+              Informes de Envio {self.informes_envio}""")
+        
