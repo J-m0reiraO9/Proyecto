65f80374899f7fb8cae06572fbecc2f549bb7f54
Juan Moreira <j.moreira@correo.unimet.edu.ve>
2024-11-10 22:44:22 -0400

    Update Pagos.py

--- Proyecto.py/Pagos.py
+++ Proyecto.py/Pagos.py
@@ -4,14 +4,17 @@ from dataclasses import dataclass
 class Pagos:
     #Constructor de la clase Pagos
     
-    pago_cliente: str
+    
+    cliente: str
+    cedula: int
     monto: float 
     moneda: str
     tipo_de_pago: str
     fecha: str
 
-    def __init__(self, pago_cliente, monto, moneda, Tipo_de_pago, fecha):
-        self.pago_cliente = pago_cliente
+    def __init__(self, cliente, cedula, monto, moneda, Tipo_de_pago, fecha):
+        self.cliente = cliente
+        self.cedula = cedula
         self.monto = monto
         self.moneda = moneda
         self.tipo_de_pago = Tipo_de_pago
@@ -20,10 +23,10 @@ class Pagos:
     def show_attr(self):
         print(f"""
               Cliente que realizo el pago = {self.pago_cliente}
+              Cedula = {self.cedula}
               Monto = {self.monto}
               Moneda = {self.moneda}
               Tipo de Pago = {self.tipo_de_pago}
               Fecha = {self.fecha}           
               """)
-        
         
