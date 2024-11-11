18823e763c401c436f9525e264d6a081dd4d2034
Juan Moreira <j.moreira@correo.unimet.edu.ve>
2024-11-10 22:46:03 -0400

    Update main.py

--- Proyecto.py/main.py
+++ Proyecto.py/main.py
@@ -1,18 +1,5 @@
 from Tienda import Tienda
-import urllib.request
-import json
-
 def main():
-    
-    url = "https://raw.githubusercontent.com/Algoritmos-y-Programacion/api-proyecto/main/products.json"
-    with urllib.request.urlopen(url) as response:
-        if response.status == 200:
-            data = response.read()  # Leer los datos
-            json_data = json.loads(data)  # Convertir a diccionario
-            #print(json_data) 
-        else:
-            print(f"Error: {response.status}")
-    
     tienda = Tienda()
     tienda.app()
 
