12bbbc72ca8da27ca91a330993d54991a1ea6aff
Juan Moreira <j.moreira@correo.unimet.edu.ve>
2024-11-10 22:45:36 -0400

    Update Ventas.py

--- Proyecto.py/Ventas.py
+++ Proyecto.py/Ventas.py
@@ -1,18 +1,19 @@
 #Constructor, metodos y atributo de la clase Ventas
-
 from dataclasses import dataclass
 @dataclass
 class Venta:
     #Constructor de la clase Venta
     
     compra: str
+    fecha: str
     productos: str
     cantidad: int
     pago: float
     envio: str
     
-    def __init__(self, compra, productos, cantidad, pago, envio, ):
+    def __init__(self, compra, fecha, productos, cantidad, pago, envio, ):
         self.compra = compra
+        self.fecha = fecha
         self.productos = productos
         self.cantidad = cantidad
         self.pago = pago
@@ -21,21 +22,11 @@ class Venta:
     def show(self):
         print(f"""
               Compra = {self.compra}
+              Fecha = {self.fecha}
               Productos = {self.productos}
               Cantidad = {self.cantidad}
               Pago = {self.pago}
               Envio = {self.envio}
               """)
-@dataclass
-class Total(Venta):
-    #Constructor de la clase Total, clase hija de venta
-    iva: float
-    subtotal: float
-    IGTF: float
 
-    def __init__(self, compra, productos, cantidad, pago, envio, iva, subtotal, IGTF):
-        super().__init__(compra, productos, cantidad, pago, envio)
-        self.iva = 0.16
-        self.subtotal = subtotal
-        self.IGTF = 0.03
         
