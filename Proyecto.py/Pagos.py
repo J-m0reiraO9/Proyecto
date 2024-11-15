709721c0648794811ac53513439a3fd9bbfbb5be
Juan Moreira <j.moreira@correo.unimet.edu.ve>
2024-11-15 08:10:10 -0400

    Update Pagos.py

--- Proyecto.py/Pagos.py
+++ Proyecto.py/Pagos.py
@@ -7,15 +7,15 @@ class Pagos:
     
     cliente: str
     cedula: int
-    monto: float 
+    monto_pago: float 
     moneda: str
     tipo_de_pago: str
     fecha: str
 
-    def __init__(self, cliente, cedula, monto, moneda, Tipo_de_pago, fecha):
+    def __init__(self, cliente, cedula, monto_pago, moneda, Tipo_de_pago, fecha):
         self.cliente = cliente
         self.cedula = cedula
-        self.monto = monto
+        self.monto_pago = monto_pago
         self.moneda = moneda
         self.tipo_de_pago = Tipo_de_pago
         self.fecha = fecha
@@ -24,7 +24,7 @@ class Pagos:
         print(f"""
               Cliente que realizo el pago = {self.pago_cliente}
               Cedula = {self.cedula}
-              Monto = {self.monto}
+              Monto = {self.monto_pago}
               Moneda = {self.moneda}
               Tipo de Pago = {self.tipo_de_pago}
               Fecha = {self.fecha}           
